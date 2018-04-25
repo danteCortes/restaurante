5a6d999f1716e4e709638b6c8072b4b38501b178
Dante Cortés <dante.e.cortes@gmail.com>
2018-04-25 18:55:40 -0500

    menu cajero

--- resources/views/cajero/inicio.blade.php
+++ resources/views/cajero/inicio.blade.php
@@ -9,6 +9,13 @@
     <b class="arrow"></b>
   </li>
   <li class="">
+    <a href="{{url('cajero')}}">
+      <i class="menu-icon fa fa-cutlery"></i>
+      <span class="menu-text"> Ver Pedidos </span>
+    </a>
+    <b class="arrow"></b>
+  </li>
+  <li class="">
     <a href="#">
       <i class="menu-icon fa fa-ban"></i>
       <span class="menu-text"> Terminar </span>
